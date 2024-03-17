db7beb20d9ded6019ceaddab76c0412c1e1bb6a3
JohnGirald92 <jotas1592@gmail.com>
2024-03-16 19:35:18 -0500

    Cambios en el código

--- final/Menu.cs
+++ final/Menu.cs
@@ -5,20 +5,20 @@ namespace itm.csharp.basic
        public static void MostrarMenu()
         {
             Console.WriteLine("1. Determinar si un número es positivo, negativo o cero.");
-            Console.WriteLine("2. Operaciones básicas entre dos números.");
-            Console.WriteLine("3. Calcular área y perímetro de un rectángulo.");
-            Console.WriteLine("4. Determinar si un año es bisiesto.");
-            Console.WriteLine("5. Encontrar el mayor de tres números.");
-            Console.WriteLine("6. Calcular el factorial de un número.");
+            Console.WriteLine("2. Muestra el mayor Número ingresado.");
+            Console.WriteLine("3. Calcular el cuadrado de un número.");
+            Console.WriteLine("4. Calcular el área de un círculo.");
+            Console.WriteLine("5. Determina que día de la semana es.");
+            Console.WriteLine("6. Determinar si el salario mensual debe pagar impuesto del 10%.");
             Console.WriteLine("7. Realizar una división.");
             Console.WriteLine("8. Sumar números impares entre 1 y 100.");
             Console.WriteLine("9. Operaciones con fracciones.");
-            Console.WriteLine("10. Comportamiento de vehículos.");
-            Console.WriteLine("11. Ingresar 5 números válidos.");
-            Console.WriteLine("12. Animales hablando.");
-            Console.WriteLine("13. Categorizar por edad.");
-            Console.WriteLine("14. Agregar contactos a una agenda.");
-            Console.WriteLine("15. Operaciones bancarias.");
+            Console.WriteLine("10. Mostrar palabra en orden inverso.");
+            Console.WriteLine("11. Determinar el promedio de 3 números.");
+            Console.WriteLine("12. Determina el número mayor de cinco números.");
+            Console.WriteLine("13. Determinar si la palabra es un palíndromo.");
+            Console.WriteLine("14. Determinar si un número es par o impar.");
+            Console.WriteLine("15. Verificar si el número está dentro del rango de 0 a 10.");
             Console.WriteLine("Ingrese el número del ejercicio que desea ejecutar:");
         }
 
